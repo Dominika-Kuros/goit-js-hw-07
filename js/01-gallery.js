0b12286da355b599c8eaf2ed0fa475a0fcf7a9f9
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-02-27 22:20:17 +0100

    zadanie 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,16 +2,16 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
- const markup = createItemsMarkup(galleryItems);
- gallery.insertAdjacentHTML("beforeend", markup);
+const markup = createItemsMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", markup);
 
 
 
- function createItemsMarkup(item) {
- return galleryItems
+function createItemsMarkup(item) {
+return galleryItems
    .map(({ preview, original, description }) => {
      return `<div class="gallery__item">
- <a class="gallery__link" href="${original.value}">
+ <a class="gallery__link" href="${original}">
  <img
  class="gallery__image"
  src="${preview}"
